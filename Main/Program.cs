f446053296d14432c7aee053cf345070d381a8e0
calexandre <calexandre@talentsoft.com>
2022-03-14 10:32:08 +0100

    main improvement

--- Main/Program.cs
+++ Main/Program.cs
@@ -12,17 +12,23 @@ namespace Main
                 Console.Clear();
                 DisplayDesignPatternTypes();
                 var userChoice = Console.ReadLine();
+                Console.Clear();
 
                 if (!int.TryParse(userChoice, out var options) || !DesignPatternType.ContainsKey(options))
                 {
                     Console.WriteLine("Please choose a valid Option !!!");
-
                 }
                 else
                 {
                     if (options == 0)
                     {
                         ContinueRunning = false;
+                        Console.WriteLine("The program will finish !!!");
+                    }
+                    else
+                    {
+                        var designPatternType = DesignPatternType[options];
+                        Console.WriteLine($" You choose the following design Pattern Type => {designPatternType.Name}");
                     }
                 }
 
@@ -34,21 +40,56 @@ namespace Main
 
         private static void DisplayDesignPatternTypes()
         {
-            Console.WriteLine("Please choose you design pattern type : ");
+            Console.WriteLine("Please choose a design pattern type : ");
             foreach (var item in DesignPatternType)
             {
-                Console.WriteLine($"{item.Key} => {item.Value}");
+                Console.WriteLine($"{item.Key} => {item.Value.Name}");
             }
         }
 
         private static bool ContinueRunning = true;
-        private static readonly Dictionary<int, string> DesignPatternType = new Dictionary<int, string>
+        private static readonly Dictionary<int, DesignPatternType> DesignPatternType = new Dictionary<int, DesignPatternType>
         {
-            { 0, "End Program" },
-            { 1, "Comportement" },
-            { 2, "Construction" },
-            { 3, "Others"},
-            { 4, "Structuration"}
+            {
+                0,
+                new DesignPatternType
+                {
+                    Name = "End Program",
+                    DesignPatterns = null,
+                }
+            },
+            {
+                1,
+                new DesignPatternType
+                {
+                    Name = "Comportement",
+                    DesignPatterns = new Dictionary<int, DesignPattern>(),
+                }
+            },
+            {
+                2,
+                new DesignPatternType
+                {
+                    Name = "Construction",
+                    DesignPatterns = new Dictionary<int, DesignPattern>(),
+                }
+            },
+            {
+                3,
+                new DesignPatternType
+                {
+                    Name = "Others",
+                    DesignPatterns = new Dictionary<int, DesignPattern>(),
+                }
+            },
+            {
+                4,
+                new DesignPatternType
+                {
+                    Name = "Structuration",
+                    DesignPatterns = new Dictionary<int, DesignPattern>(),
+                }
+            },
         };
     }
 }
